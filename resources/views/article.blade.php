f93a534ba597a91c51e38e2015e4b2e39c21c00c
CDamasse <charles.damasse@gmail.com>
2017-12-23 11:08:01 +0100

    #2

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -8,17 +8,7 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <h2>
-                            {{ $post->title }}
 
-                            @if($post->user)
-                                Auteur: {{ $post->user->name }}
-                            @endif
-                        </h2>
-                    </div>
-                    <p>
-                        {{ $post->body }}
-                    </p>
                 </div>
             </div>
         </div>

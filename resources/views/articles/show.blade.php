5fa400d8dc2de903fed50ae7e2f63f3a571b5952
Roi-Issam <33629850+Roi-Issam@users.noreply.github.com>
2018-01-07 18:42:29 +0100

    #C

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -5,5 +5,56 @@
     <h1>{{ $articles->title }}</h1>
     <p>{{ $articles->body }}</p>
     <img src="{{ asset('storage/uploads/'.$articles->image) }}" class="img-fluid" alt="Responsive image" style="width: 100%;">
+
+    <div class="comments">
+
+        <ul class="list-group">
+
+            @foreach ($post->comments as $comment)
+
+                <li class="list-group-item">
+
+                    <strong>
+
+                        {{ $comment->created_at->diffForHumans() }}: &nbsp ;
+
+                    </strong>
+
+                    {{ $comment->body }}
+                </li>
+
+            @endforeach
+        </ul>
+
+    </div>
+
+    <div class="card">
+
+        <div class="card-block">
+
+            <form>
+
+                <div class="form-group">
+                     <textarea name="body" placeholder="Your comment here." class="form-control">
+                     </textarea>
+                </div>
+
+
+                <div class="form-group">
+
+                    <button type="submit" class="btn btn-primary"> Add Comment</button>
+
+                </div>
+
+            </form>
+        </div>
+
+    </div>
+
+
+
+
 @endsection
+
+
 
